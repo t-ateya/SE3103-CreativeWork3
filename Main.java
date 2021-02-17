30222dfab3937eab477b0ad3104758ac7774f91f
Terence Ateya <ateyaterence@gmail.com>
2021-02-16 21:14:00 -0600

    true

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
+import javax.swing.JFrame;
+
+import view.MenuScreen;
+
 public class Main {
 	public static void main(String[]args){
+		JFrame window = new JFrame();
+
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setLocation(400, 100);
+		window.setTitle("EMPLOYEE VOUCHER SYSTEM");
+
+		var menu = new MenuScreen(window);
+		menu.init();
+
+		window.pack();
+		window.setVisible(true);
 		
 	}
 	
